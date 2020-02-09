bd17f3c5fd3060285a91a249551630a83601d55a
Kasaract <garynguyen2018@gmail.com>
2020-02-09 08:07:20 -0500

    Clean up

--- fast-trashion/navigation/MainTabNavigator.js
+++ fast-trashion/navigation/MainTabNavigator.js
@@ -9,69 +9,73 @@ import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const config = Platform.select({
-  web: { headerMode: 'screen' },
-  default: {},
+	web: { headerMode: 'screen' },
+	default: {}
 });
 
 const HomeStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  config
+	{
+		Home: HomeScreen
+	},
+	config
 );
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
+	tabBarLabel: 'Home',
+	tabBarIcon: ({ focused }) => (
+		<TabBarIcon
+			focused={focused}
+			name={
+				Platform.OS === 'ios'
+					? `ios-information-circle${focused ? '' : '-outline'}`
+					: 'md-information-circle'
+			}
+		/>
+	)
 };
 
 HomeStack.path = '';
 
 const LinksStack = createStackNavigator(
-  {
-    Links: LinksScreen,
-  },
-  config
+	{
+		Links: LinksScreen
+	},
+	config
 );
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
+	tabBarLabel: 'Links',
+	tabBarIcon: ({ focused }) => (
+		<TabBarIcon
+			focused={focused}
+			name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+		/>
+	)
 };
 
 LinksStack.path = '';
 
 const SettingsStack = createStackNavigator(
-  {
-    Settings: SettingsScreen,
-  },
-  config
+	{
+		Settings: SettingsScreen
+	},
+	config
 );
 
 SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
-  ),
+	tabBarLabel: 'Settings',
+	tabBarIcon: ({ focused }) => (
+		<TabBarIcon
+			focused={focused}
+			name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+		/>
+	)
 };
 
 SettingsStack.path = '';
 
 const tabNavigator = createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
+	HomeStack
 });
 
 tabNavigator.path = '';
--- fast-trashion/screens/HomeScreen.js
+++ fast-trashion/screens/HomeScreen.js
@@ -1,175 +1,117 @@
 import * as WebBrowser from 'expo-web-browser';
 import React, { useRef, useState, useEffect } from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-import { Camera } from 'expo-camera';
 import CameraInput from '../CameraInput.js';
-
-import { MonoText } from '../components/StyledText';
 import * as Permissions from 'expo-permissions';
 
 export default function HomeScreen() {
+	const [status, setStatus] = useState(false);
 
-  const [status, setStatus] = useState(false)
-console.log(status)
-  useEffect(()=>{
-    Permissions.askAsync(Permissions.CAMERA).then(({status})=>{setStatus(status==='granted')})
-  }, [])
+	useEffect(() => {
+		Permissions.askAsync(Permissions.CAMERA).then(({ status }) => {
+			setStatus(status === 'granted');
+		});
+	}, []);
 
-const inputRef = useRef(null)
+	// const inputRef = useRef(null);
 
-  // var statusGranted;
-  // Permissions.askAsync(Permissions.CAMERA).then(({status})=>{statusGranted=(status==="granted")}).then(console.log(statusGranted));
-  
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.contentContainer}
-        contentContainerStyle={styles.contentContainer}>
+	// var statusGranted;
+	// Permissions.askAsync(Permissions.CAMERA).then(({status})=>{statusGranted=(status==="granted")}).then(console.log(statusGranted));
 
-        <View  allow="microphone; camera;"> 
-          {/* <input ref={inputRef} type="text" /> */}
-          <CameraInput/>
-        </View>
-
-      </ScrollView>
-
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<ScrollView
+				style={styles.contentContainer}
+				contentContainerStyle={styles.contentContainer}
+			>
+				<View allow="microphone; camera;">
+					{/* <input ref={inputRef} type="text" /> */}
+					<CameraInput />
+				</View>
+			</ScrollView>
+		</View>
+	);
 }
 
 HomeScreen.navigationOptions = {
-  header: null,
+	header: null
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff'
+	},
+	contentContainer: {
+		paddingTop: 0
+	},
+	welcomeContainer: {
+		alignItems: 'center',
+		marginTop: 10,
+		marginBottom: 20
+	},
+	welcomeImage: {
+		width: 100,
+		height: 80,
+		resizeMode: 'contain',
+		marginTop: 3,
+		marginLeft: -10
+	},
+	getStartedContainer: {
+		alignItems: 'center',
+		marginHorizontal: 50
+	},
+	homeScreenFilename: {
+		marginVertical: 7
+	},
+	codeHighlightText: {
+		color: 'rgba(96,100,109, 0.8)'
+	},
+	codeHighlightContainer: {
+		backgroundColor: 'rgba(0,0,0,0.05)',
+		borderRadius: 3,
+		paddingHorizontal: 4
+	},
+	getStartedText: {
+		fontSize: 17,
+		color: 'rgba(96,100,109, 1)',
+		lineHeight: 24,
+		textAlign: 'center'
+	},
+	tabBarInfoContainer: {
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+		right: 0,
+		...Platform.select({
+			ios: {
+				shadowColor: 'black',
+				shadowOffset: { width: 0, height: -3 },
+				shadowOpacity: 0.1,
+				shadowRadius: 3
+			},
+			android: {
+				elevation: 20
+			}
+		}),
+		alignItems: 'center',
+		backgroundColor: '#fbfbfb',
+		paddingVertical: 20
+	},
+	tabBarInfoText: {
+		fontSize: 17,
+		color: 'rgba(96,100,109, 1)',
+		textAlign: 'center'
+	},
+	navigationFilename: {
+		marginTop: 5
+	},
+	helpContainer: {
+		marginTop: 15,
+		alignItems: 'center'
+	},
+	helpLink: {
+		paddingVertical: 15
+	}
 });
